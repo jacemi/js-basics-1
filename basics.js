f69065d6325b94f82d470f5f45eb2c076321faa1
Jace Mitsuka <PJace@BigMac.local>
2018-03-05 20:01:14 -1000

    problem 3

--- basics.js
+++ basics.js
@@ -17,6 +17,8 @@ var person = {
  * if it should be true if your person object is at least 16 years old
  */
 
+var canDrive = true;
+
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
